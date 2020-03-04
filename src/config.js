4c261192971f96a4e81b95b1cf5cee2a8b16e2ef
Ridwan <ridwan@bmsit.ac.in>
2020-03-04 23:31:47 +0530

    Setting up React

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+    s3: {
+      REGION: "us-east-2",
+      BUCKET: "rdwn-note"
+    },
+    apiGateway: {
+      REGION: "us-east-2",
+      URL: "https://yf5hpkfgs0.execute-api.us-east-2.amazonaws.com/dev"
+    },
+    cognito: {
+      REGION: "us-east-2",
+      USER_POOL_ID: "us-east-2_q1LDZdjrw",
+      APP_CLIENT_ID: "1dtvf4ikka4cdq3gaern0pcjrq",
+      IDENTITY_POOL_ID: "us-east-2:9d2946a9-af5a-497d-94ec-229eb42df7e3"
+    }
+  };
